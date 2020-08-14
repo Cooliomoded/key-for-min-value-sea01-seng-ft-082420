2b446dae754af85643417c241195342c2fa426f4
Davis Brown <cooliomoded@gmail.com>
2020-08-14 18:46:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,22 @@
 
 def key_for_min_value(name_hash)
   lowest_value = 99999
+<<<<<<< HEAD
   lowest_key = nil
+=======
+  lowest_key = ""
+>>>>>>> 41d51e6b30b8a3b449d439e586618ba0a04b49a0
   name_hash.each do |key, value|
     while value < lowest_value do
       lowest_value = value
       lowest_key = key
+<<<<<<< HEAD
+=======
+      while name_hash.count == 0 do
+        lowest_key = nil
+        end
+      end
+>>>>>>> 41d51e6b30b8a3b449d439e586618ba0a04b49a0
     end
   end
 lowest_key
